5d9cf01bec95906d3ba89f814294af788127ca97
roarijo89 Tweaks <roarijo89@gmail.com>
2021-03-13 13:01:19 -0500

    Dont touch my shit bitch woo

---
+++ policy page/js/main.js
@@ -0,0 +1,14 @@
+function pop(){
+    Swal.fire({
+        title: "Designed",
+  text: " roarijo89 & Jailed creations",
+  icon: "info",
+  confirmButtonText: "Thank u",
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      });
+  }
